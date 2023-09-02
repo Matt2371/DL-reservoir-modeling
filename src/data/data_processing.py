Fixed bug where test set is wrong dimension

src/data/data_processing.py
@@ -133,6 +133,6 @@ class processing_pipeline():
         # 4. Convert data to PyTorch tensors (from numpy array)
         ts_train, ts_val, ts_test = torch.tensor(df_train, dtype=torch.float), torch.tensor(df_val, dtype=torch.float), torch.tensor(df_test, dtype=torch.float)
         # 5. Split data into chunks and pad the remainder (for training and validation sets)
-        ts_train, ts_val = split_and_pad(ts_train), split_and_pad(ts_val)
+        ts_train, ts_val, ts_test = split_and_pad(ts_train), split_and_pad(ts_val), split_and_pad(ts_test)
 
         return ts_train, ts_val, ts_test

tests/test_predict_model.py
@@ -16,5 +16,5 @@ class test_predict_model(unittest.TestCase):
         # Get outputs
         output_y_hat, output_y = flatten_rm_pad(y_hat=input_y_hat, y=input_y, pad_value=-1)
 
-        np.testing.assert_almost_equal(expected_y.numpy(), output_y.numpy())
-        np.testing.assert_almost_equal(expected_y_hat.numpy(), output_y_hat.numpy())
+        np.testing.assert_almost_equal(expected_y, output_y)
+        np.testing.assert_almost_equal(expected_y_hat, output_y_hat)