befe6350e1baa210d7e6e231080e119278e8dde7
Matt2371 <mtwchen@ucdavis.edu>
2024-02-08 16:23:01 -0800

    Added MCLSTM experiment
    - added mclstm model experiments
    - modified src.models.train_model.training_loop() to account for MCLSTM (separate mass and auxillary input)

---
+++ experiments/8a_train_mclstm.py
@@ -0,0 +1,97 @@
+## TRAIN A MC-LSTM FOR SHASTA ##
+
+# Modifications to data processing: Separated mass (X_m) and auxillary (X_a) inputs
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from math import floor
+import os
+import copy
+
+from src.data.data_processing import *
+from src.data.data_fetching import *
+from src.models.model_zoo import *
+from src.models.predict_model import *
+from src.models.train_model import *
+from src.models.hyperparameter_tuning import *
+from src.models import mclstm
+
+def data_processing(name, transform_type, train_frac=0.6, val_frac=0.2, test_frac=0.2, left='1944-01-01', right='2022-12-31', log_names=[], return_scaler=False):
+    """
+    Run data processing pipeline.
+    Params:
+    name -- str, name of reservoir to read
+    transform_type -- str, in preprocessing, whether to 'standardize' or 'normalize' the data.
+    left -- str (YYYY-MM-DD), beginning boundary of time window
+    right -- str (YYYY-MM-DD), end boundary of time window
+    log_names -- list of column names (str) to take log of before running rest of pipeline. E.g. ['inflow', 'outflow', 'storage']
+    return_scaler -- bool, whether or not to return src.data.data_processing.time_scaler() object
+    """
+
+    # Read in data, columns are [inflow, outflow, storage]
+    df = usbr_fetch_data(name=name, vars=['inflow', 'outflow', 'storage'])
+    # Add day of the year (doy) as another column
+    df['doy'] = df.index.to_series().dt.dayofyear
+    # Select data window
+    df = df[left:right].copy()
+
+    # Take log of df columns that are in log_names
+    for column_name in df.columns:
+        if column_name in log_names:
+            df[column_name] = np.log(df[column_name])
+        else:
+            continue
+
+    # Run data processing pipeline
+    pipeline = processing_pipeline(train_frac=train_frac, val_frac=val_frac, test_frac=test_frac, chunk_size=3*365, pad_value=-1, transform_type=transform_type, fill_na_method='mean')
+    # Train/val/test tensors of shape (#chunks, chunksize, [inflow, outflow, storage, doy])
+    ts_train, ts_val, ts_test = pipeline.process_data(df) 
+
+    # Separate inputs(X) and targets (y)
+    X_train_a, X_val_a, X_test_a = ts_train[:, :, [3]], ts_val[:, :, [3]], ts_test[:, :, [3]] # auxillary input = doy
+    X_train_m, X_val_m, X_test_m = ts_train[:, :, [0]], ts_val[:, :, [0]], ts_test[:, :, [0]] # mass input = inflow
+
+    # select outflow as target feature
+    y_train, y_val, y_test = ts_train[:, :, [1]], ts_val[:, :, [1]], ts_test[:, :, [1]]
+
+    if return_scaler:
+        return (X_train_m, X_train_a, y_train), (X_val_m, X_val_a, y_val), (X_test_m, X_test_a, y_test), pipeline.scaler
+    else:
+        return (X_train_m, X_train_a, y_train), (X_val_m, X_val_a, y_val), (X_test_m, X_test_a, y_test)
+    
+
+def main():
+    ## Data processing
+    data_result = data_processing(name='Shasta', transform_type='standardize')
+    # Create PyTorch Dataset and Dataloader
+    dataset_train_res, dataset_val_res = (TensorDataset(*data_result[0]), 
+                                        TensorDataset(*data_result[1]))
+    dataloader_train_res, dataloader_val_res = (DataLoader(dataset_train_res, batch_size=1, shuffle=False), 
+                                                DataLoader(dataset_val_res, batch_size=1, shuffle=False))
+
+    ## Model training
+    # Instantiate model and optimizer
+    torch.manual_seed(0)
+    criterion = nn.MSELoss()
+    mc_lstm = mclstm.MassConservingLSTM(in_dim=1, aux_dim=1, out_dim=1, batch_first=True)
+    optimizer = optim.Adam(mc_lstm.parameters(), lr=0.001)
+    # Run training loop (finetune model to shasta)
+    train_losses, val_losses = training_loop(model=mc_lstm, criterion=criterion, optimizer=optimizer, 
+                                            patience=10, dataloader_train=dataloader_train_res, 
+                                            dataloader_val=dataloader_val_res, epochs=300, mclstm=True)
+    # Save trained model
+    torch.save(mc_lstm.state_dict(), 'src/models/saved_models/mc_lstm.pt')
+
+    # Plot train/validation plot
+    plot_train_val(train_losses=train_losses, val_losses=val_losses)
+    plt.savefig('jobs/mclstm_training.png')
+    return
+
+# run script
+if __name__ == '__main__':
+    main()
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -60,6 +60,39 @@ def train_one_epoch(model, criterion, optimizer, dataloader_train, return_cell=T
     avg_loss = total_loss / len(dataloader_train)
     return avg_loss
 
+def train_one_epoch_mclstm(model, criterion, optimizer, dataloader_train):
+    """
+    Same as train_one_epoch() except for MC-LSTM, where mass input and
+    auxillary input need to be separated.
+    Return training loss (averaged for over each minibatch) for the epoch
+    """
+
+    # training loop
+    total_loss = 0
+    model.train() # set model to training mode
+    for inputs_m, inputs_a, targets in dataloader_train:
+        optimizer.zero_grad()
+
+        # Forward pass
+        outputs, c = model(inputs_m, inputs_a)
+
+        # Apply mask on targets/outputs
+        mask = create_mask(targets)
+        masked_outputs = outputs * mask
+        masked_targets = targets * mask
+
+        # Calculate loss with masked targets
+        loss = criterion(masked_outputs, masked_targets)
+
+        # Backward pass and optimization step
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    avg_loss = total_loss / len(dataloader_train)
+    return avg_loss
+
 def val_one_epoch(model, criterion, dataloader_val, return_cell=True):
     """
     Calculate validation loss for a given epoch
@@ -93,6 +126,32 @@ def val_one_epoch(model, criterion, dataloader_val, return_cell=True):
         avg_val_loss = total_val_loss / len(dataloader_val)
     return avg_val_loss
 
+def val_one_epoch_mclstm(model, criterion, dataloader_val):
+    """
+    Same as val_one_epoch() except for MC-LSTM, where mass input and
+    auxillary input need to be separated.
+    Return validation loss (averaged for over each minibatch) for the epoch
+    """
+
+    model.eval()  # Set the model to evaluation mode
+    with torch.no_grad():
+        total_val_loss = 0
+        for val_inputs_m, val_inputs_a, val_targets in dataloader_val:
+            # forward pass
+            val_outputs, c = model(val_inputs_m, val_inputs_a)
+
+            # apply mask on targets/outputs
+            mask = create_mask(val_targets)
+            val_masked_outputs = val_outputs * mask
+            val_masked_targets = val_targets * mask
+
+            # evaluate validation loss
+            val_loss = criterion(val_masked_outputs, val_masked_targets)
+            total_val_loss += val_loss.item()
+
+        avg_val_loss = total_val_loss / len(dataloader_val)
+    return avg_val_loss
+
 class EarlyStopper:
     """Implement EarlyStopper callback to prevent overfitting"""
     def __init__(self, patience=1, min_delta=0):
@@ -111,7 +170,7 @@ class EarlyStopper:
                 return True
         return False
     
-def training_loop(model, criterion, optimizer, patience, dataloader_train, dataloader_val, epochs):
+def training_loop(model, criterion, optimizer, patience, dataloader_train, dataloader_val, epochs, mclstm=False):
     """
     Run full training loop with early stopping.
     Params:
@@ -122,6 +181,7 @@ def training_loop(model, criterion, optimizer, patience, dataloader_train, datal
     dataloader_train -- PyTorch Dataloader for training data
     dataloader_val -- PyTorch Dataloader for validation data
     epochs -- maximum number of training epochs
+    mclstm -- bool, whether or not using mclstm architecture (separate auxillary and mass input)
 
     Returns:
     train_losses -- average training losses for each epoch
@@ -134,10 +194,16 @@ def training_loop(model, criterion, optimizer, patience, dataloader_train, datal
     early_stopper = EarlyStopper(patience=patience) # instantiate early stopper
     for epoch in tqdm(range(num_epochs), desc='Training epochs: '):
         model.train() # set model1 to training mode
-        train_loss = train_one_epoch(model, criterion, optimizer, dataloader_train)
-        train_losses.append(train_loss)
-        val_loss = val_one_epoch(model, criterion, dataloader_val)
-        val_losses.append(val_loss)
+        if mclstm:
+            train_loss = train_one_epoch_mclstm(model, criterion, optimizer, dataloader_train)
+            train_losses.append(train_loss)
+            val_loss = val_one_epoch_mclstm(model, criterion, dataloader_val)
+            val_losses.append(val_loss)
+        else:
+            train_loss = train_one_epoch(model, criterion, optimizer, dataloader_train)
+            train_losses.append(train_loss)
+            val_loss = val_one_epoch(model, criterion, dataloader_val)
+            val_losses.append(val_loss)
 
         if early_stopper.early_stop(val_loss):
             break
